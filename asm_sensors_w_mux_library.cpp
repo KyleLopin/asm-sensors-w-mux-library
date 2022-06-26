afd6da417eb953fd8463c30e7d59cf6ca1a3796e
KyleLopin <KyleLopin@gmail.com>
2022-06-26 15:43:47 +0700

    Added files

---
+++ asm_sensors_w_mux_library.cpp
@@ -0,0 +1,102 @@
+#include "asm_sensors_w_mux_library.h"
+#include "Arduino.h"
+
+//Constructor
+SpectroDesktop::SpectroDesktop()
+{
+}
+
+// Initialize the device by:
+// 1) checking for the qwiic mux
+// 2) look for any and all as7262/as7263/as7265x sensor over qwiic
+// Returns false if no sensor
+boolean SpectroDesktop::begin() {
+    QWIICMUX myMux;
+    button.begin();  // use this to represent every button
+
+    use_mux = myMux.begin();  // set boolean if the device has a i2c mux or not
+    // begin turns all ports off
+    Serial.println("Has a mux");
+    //    for (int i=0; i <= 7; i++) {
+    //      myMux.enablePort(i);
+    //      if (button.isConnected()) {
+    //        Serial.print("Button on port: ");
+    //        Serial.println(i);
+    //        button.clearEventBits();  // if the button has been pressed before, clear it now
+    //      }
+    get_sensor_info();
+    return (true);
+}
+
+void SpectroDesktop::get_sensor_info() {
+    if (use_mux) {
+        Serial.println("Have mux");
+        for (int i = 0; i <= 7; i++) {
+            myMux.enablePort(i);
+            int avail = check_channel();
+            if (avail) {
+                sensor_type[i] = get_sensor_type(i);
+            }
+            myMux.disablePort(i);
+        }
+    }
+    else {
+        Serial.println("No mux");
+        int avail = check_channel();
+        if (avail) {
+            sensor_type[0] = get_sensor_type(0);  // just put in a place holder for the channel
+        }
+    }
+    Serial.println("End Setup");
+}
+
+boolean SpectroDesktop::check_channel() {
+    boolean available_channel = false;
+    Wire.requestFrom(AS726X_ADDR, 1);
+    available_channel = Wire.available();
+    return available_channel;
+}
+
+byte SpectroDesktop::get_sensor_type(int channel) {
+    byte _sensor_type = NO_SENSOR;
+    if (as726x.begin(Wire) == true) {
+        as726x.setIntegrationTime(150);
+        as726x.setBulbCurrent(0b11);
+    }
+    uint8_t hw_type = as726x.getVersion();
+    Serial.print("Hardware type: 0x");
+    Serial.println(hw_type, HEX);
+
+    if (hw_type == AS7262_CODE) {
+        _sensor_type = AS7262_SENSOR;
+        Serial.print("AS7262 device attached to port: ");
+        Serial.print(channel); Serial.print("|  ");
+    }
+    else if (hw_type == AS7263_CODE) {
+        _sensor_type = AS7263_SENSOR;
+        Serial.print("AS7263 device attached to port: ");
+        Serial.print(channel); Serial.print("|  ");
+    }
+    else if (hw_type == AS7265X_CODE) {
+        _sensor_type = AS7265X_SENSOR;
+        as7265x.begin();
+        as7265x.setBulbCurrent(AS7265X_LED_CURRENT_LIMIT_12_5MA, AS7265x_LED_WHITE);
+        as7265x.setBulbCurrent(AS7265X_LED_CURRENT_LIMIT_12_5MA, AS7265x_LED_IR);
+        as7265x.setBulbCurrent(AS7265X_LED_CURRENT_LIMIT_12_5MA, AS7265x_LED_UV);
+        as7265x.disableIndicator();
+        as7265x.setIntegrationCycles(150);
+        Serial.print("AS7265x device attached to port: ");
+        Serial.print(channel); Serial.print("|  ");
+    }
+    if (button.isConnected() == false) {
+        Serial.println("No button attached to device.");
+    }
+    else {
+        Serial.println("Button attached to device.");
+        button.clearEventBits();  // Clear any clicks before being setup
+        button.LEDoff();
+        button.setDebounceTime(20);  // Sometime this can get messed up for some reason
+    //    println(button.getDebounceTime());
+    }
+    return _sensor_type;
+}
---
+++ asm_sensors_w_mux_library.h
@@ -0,0 +1,68 @@
+/*
+  This is a library written to allow the AMS AS7265x,
+  the AS7262 and AS7263 spectral boards to be used singly
+  or in combination with a Qwiic mux board
+  SparkFun sells these parts at its website: www.sparkfun.com
+  https://www.sparkfun.com/products/15050  - AS7265X breakout board
+  https://www.sparkfun.com/products/14347 - AS7262 breakout board
+  https://www.sparkfun.com/products/14351 - AS7263 breakout board
+  https://www.sparkfun.com/products/16784 - Qwiic Mux breakout board
+
+  Written by Kyle Vitautas Lopin @ Naresuan University, June, 2022
+
+  Development environment specifics:
+  Arduino IDE 1.8.11
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _ASM_SENSORS_W_MUX_LIBRARY_H
+#define _ASM_SENSORS_W_MUX_LIBRARY_H
+
+// Dependent Libraries
+#include <AS726X.h>  // http://librarymanager/All#Sparkfun_AS726X
+#include <SparkFun_AS7265X.h>  // http://librarymanager/All#Sparkfun_AS7265X
+#include <SparkFun_I2C_Mux_Arduino_Library.h>  // http://librarymanager/All#Sparkfun_I2C_Mux
+#include <SparkFun_Qwiic_Button.h>  // http://librarymanager/All#Sparkfun_Qwiic_Button_Switch
+#include <Wire.h>
+
+// Define statements
+// I2C info
+#define AS726X_ADDR 0x49
+#define BUTTON_ADDR 0x6F
+#define AS7262_CODE 0x3E
+#define AS7263_CODE 0x3F
+#define AS7265X_CODE 0x41
+
+// Enums and constants
+enum SENSOR : byte
+{
+	NO_SENSOR, AS7262_SENSOR, AS7263_SENSOR, AS7265X_SENSOR
+};
+
+class SpectroDesktop
+{
+public:
+
+	SpectroDesktop();
+	QWIICMUX myMux;  // Qwiic mux if it is present
+	QwiicButton button;  // this will represent EVERY button ;) so its not an array
+	AS726X as726x;  // treat all as726x the same
+	AS7265X as7265x;  // treat all as7265x the same
+	boolean begin();
+
+private:
+	boolean use_mux = false;  // flag if there is a mux or not
+	byte sensor_type[8];
+	void get_sensor_info();
+	boolean check_channel();
+	byte get_sensor_type(int channel);
+};
+
+#endif
